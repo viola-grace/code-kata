427fb346cd93e51938b9372c37fcb49ca979be7c
viola-grace <36133518+viola-grace@users.noreply.github.com>
2019-06-11 09:40:16 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{    
+    long int n;
+    scanf("%ld",&n);
+    if(n<0)
+    {
+        printf("Invalid");
+    }else if(n%2==0)
+    {
+        printf("Even");
+    }
+    else if(n%2!=){
+        printf("Odd");
+    }
+}
